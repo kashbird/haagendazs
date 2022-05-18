b467c8fc7a64b87baa0d350b0e942e519c16c6d4
Jini Pong <artisthg122@gmail.com>
2022-05-18 19:12:59 +0900

    asdf

---
+++ index.js
@@ -0,0 +1,13 @@
+let leftBtn = document.querySelector('.left');
+let rightBtn = document.querySelector('.right');
+let link = document.location.href;
+let data = ['index.html', 'main2.html', 'main3.html', 'main4.html', 'main5.html'];
+
+if(link.match('/index') == data[0]){
+  
+}
+
+
+leftBtn.addEventListener('click', ()=> {
+  leftBtn.setAttribute()
+})
